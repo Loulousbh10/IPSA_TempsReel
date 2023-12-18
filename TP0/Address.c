40654ae4b3862d7a979d251e4b0530f444f7e917
Lou RIO <lou.rio@ipsa.fr>
2023-12-18 10:56:35 +0100

    Ajout de fonctions

---
+++ TP0/Address.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+	int a = 5;
+	printf("%p\n", &a);
+}
---
+++ TP0/Sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	int a = 3;
+	int b = 5;
+	int sum = a + b;
+	
+	printf("Result = %u\n", sum);
+
+}
