593c831e92ab8bea2f67ff9f4342a0b3115a1562
Lou RIO <lou.rio@ipsa.fr>
2023-12-22 09:15:13 +0100

    Ajout Array.c

--- TP0/Array.c
+++ TP0/Array.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 
-int main(void) {
-	int blbl[] = {0, 2, 4, 6, 8};
-	int valuei;
+int main() {
+    // Create an array
+    int arr[] = {10, 20, 30, 40, 50};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
-	printf("Enter value to find : \n");
-	scanf("%d", &valuei);
-	
-	for (int i = 0; i < 5; i++) {
-    	if (valuei == blbl[i]) {
-    		printf("Iteration %d de la liste.\n", i);
-    	}
-	}	
-	return 0;
+    // Value to search for
+    int target;
+    printf("Enter value to find : \n");
+	scanf("%d", &target);
+
+    // Search for the value in the array
+    int found = 0;
+    for (int i = 0; i < size; ++i) {
+        if (arr[i] == target) {
+            found = 1;
+            break;
+        }
+    }
+
+    // Display the result
+    if (found) {
+        printf("Value %d found in the array.\n", target);
+    } else {
+        printf("Value %d not found in the array.\n", target);
+    }
+
+    return 0;
 }
+
